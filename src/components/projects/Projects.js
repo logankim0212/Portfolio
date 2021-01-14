6d13656805e83c443d89cc25b97daaff95199dc4
logankim0212 <logan.kim0212@gmail.com>
2021-01-14 13:42:39 -0500

    scroll animations added to project page

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -34,6 +34,74 @@ export default class Projects extends Component {
                 amount: 0.15
             }
         });
+
+        let panels = document.querySelectorAll(".panel");
+
+        for (let i = 1; i <= panels.length; i++) {
+            let projectTitle = document.querySelector(".project-title" + i);
+            let projectDescription = document.querySelector(".project-description" + i);
+            let projectButton = document.querySelector(".project-button" + i);
+            let projectImage = document.querySelector(".project-image" + i);
+
+            gsap.from([projectTitle], {
+                delay: 0.5,
+                duration: 0.5,
+                ease: "ease.out",
+                y: 200,
+                opacity: 0,
+                stagger: {
+                    amount: 0.15
+                },
+                scrollTrigger: {
+                    trigger: [projectTitle],
+                    start: 'top 95%',
+                    end: 'bottom 5%',
+                    toggleActions: 'restart reverse restart reverse'
+                },
+            });
+
+            gsap.from([projectDescription], {
+                duration: 1,
+                y: 100,
+                opacity: 0,
+                ease: "power3.out",
+                scrollTrigger: {
+                    trigger: [projectDescription],
+                    start: 'top 95%',/**/
+                    end: 'bottom 10%',
+                    toggleActions: 'restart reverse restart reverse'
+                },
+            });
+
+            gsap.from([projectButton], {
+                delay: 1,
+                duration: 1,
+                ease: "back.out",
+                transformOrigin: "center",
+                scaleX: .3,
+                scaleY: .3,
+                opacity: 0,
+                scrollTrigger: {
+                    trigger: [projectButton],
+                    start: 'top 95%',
+                    end: 'bottom 10%',
+                    toggleActions: 'restart reverse restart reverse'
+                },
+            });
+
+            gsap.from([projectImage], {
+                duration: 1,
+                x: 100,
+                opacity: 0,
+                ease: "power3.inOut",
+                scrollTrigger: {
+                    trigger: [projectImage],
+                    start: 'top 95%',/**/
+                    end: 'bottom 10%',
+                    toggleActions: 'restart reverse restart reverse'
+                },
+            });
+        }
     }
 
     render() {
@@ -64,35 +132,41 @@ export default class Projects extends Component {
                                 return (
                                     <div key={index} className={'panel'}>
                                         <div className={'sub-panel a'}>
-                                            <h3 className={'project-title'}>{item.title}</h3>
-                                            <p>{item.description}</p>
-                                            <a href={item.projectLink} target="_blank">
-                                                <div className="button">
-                                                    <div className="inner-button-1">
-                                                        <div className="inner-button-2">
-                                                            <div className="inner-button-3">
-                                                                <div className="inner-button-4">
-                                                                    <div className="inner-button-content">
-                                                                        <span className="button-text">View Project</span>
-                                                                        <span className="hover-button-text">View Project</span>
+                                            <div className={'line-wrap'}>
+                                                <h3 className={'project-title project-title' + (index + 1)}>{item.title}</h3>
+                                            </div>
+                                            <p className={'project-description' + (index + 1)}>{item.description}</p>
+                                            <div className={'project-button' + (index + 1)}>
+                                                <a href={item.projectLink} target="_blank" rel="noreferrer">
+                                                    <div className="button">
+                                                        <div className="inner-button-1">
+                                                            <div className="inner-button-2">
+                                                                <div className="inner-button-3">
+                                                                    <div className="inner-button-4">
+                                                                        <div className="inner-button-content">
+                                                                            <span
+                                                                                className="button-text">View Project</span>
+                                                                            <span className="hover-button-text">View Project</span>
+                                                                        </div>
                                                                     </div>
                                                                 </div>
                                                             </div>
                                                         </div>
                                                     </div>
-                                                </div>
-                                            </a>
+                                                </a>
+                                            </div>
                                         </div>
                                         <div className={'sub-panel b'}>
-                                            <img className={'project-image'} src={item.imageLink} alt={'screenshots of ' + item.title} />
+                                            <img className={'project-image project-image' + (index + 1)}
+                                                 src={item.imageLink} alt={'screenshots of ' + item.title}/>
                                         </div>
                                     </div>
                                 )
                             })
+                            }
+                            </div>
+                            </section>
+                            </div>
+                            )
                         }
-                    </div>
-                </section>
-            </div>
-        )
-    }
-}
+                        }
